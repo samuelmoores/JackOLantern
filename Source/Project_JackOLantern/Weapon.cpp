21238b7c5afaece01f1d9b87fbf26e1cdba000c5
samuelmoores <samueldmoores@gmail.com>
2024-01-26 12:50:25 -0800

    decrease distance for ghost to start shooting

--- Source/Project_JackOLantern/Weapon.cpp
+++ Source/Project_JackOLantern/Weapon.cpp
@@ -9,7 +9,13 @@ AWeapon::AWeapon()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	Tags.Add("Weapon");
+
+}
+
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	Mesh->SetWorldScale3D(FVector(2.0f, 2.0f, 2.0f));
 
 }
 
@@ -21,6 +27,11 @@ void AWeapon::Tick(float DeltaSeconds)
 	{
 		Mesh->SetRelativeRotation(FRotator(Mesh->GetComponentRotation().Pitch, Mesh->GetComponentRotation().Yaw + (DeltaSeconds * 100.0f), Mesh->GetComponentRotation().Roll));
 	
+	}
+	else
+	{
+		Mesh->SetWorldScale3D(FVector::One());
+
 	}
 }
 
